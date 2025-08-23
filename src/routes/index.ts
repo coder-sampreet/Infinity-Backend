1eae26274469505215a29b0529fb6a3f60e75943
coder-sampreet <officialsampreet02@gmail.com>
2025-08-24 01:53:56 +0530

    swagger configuration

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import SystemRoutes from "./system.routes.js";
+import docsRoutes from "./docs.routes.js";
 
 const router = express.Router();
 
 router.use("/", SystemRoutes);
+router.use("/", docsRoutes);
 
 export default router;
